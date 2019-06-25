f251ce4ed392e24cf2932e307a660ba9c769c3da
Pascal Luginbuehl <pascal.luginbuehl@bluewin.ch>
2019-06-25 23:32:31 +0200

    Submit button upgraded

--- src/components/Validator/Submit.tsx
+++ src/components/Validator/Submit.tsx
@@ -3,15 +3,33 @@ import { InjectedIntlProps, injectIntl, FormattedMessage } from 'react-intl'
 import { WithFormContext, withValidator } from '.'
 import { Button, Grid } from '@material-ui/core'
 import Loading from '../Loading'
+import { createStyles, Theme, WithStyles, withStyles, Fab, CircularProgress } from '@material-ui/core'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import withWidth, { WithWidth, isWidthUp } from '@material-ui/core/withWidth';
 
+const styles = (theme: Theme) =>
+  createStyles({
+    fab: {
+      position: 'fixed',
+      bottom: theme.spacing(2),
+      right: theme.spacing(3),
+    },
+    rightAlign: {
+      margin: theme.spacing(2),
+      width: "100%",
+      textAlign: "right",
+    }
+  })
 
-interface Props extends InjectedIntlProps, WithFormContext {
-  onSubmit: () => Promise<any>
-  label?: string
-}
 interface State {
   saveAwait: Promise<any> | null
 }
+
+interface Props extends WithStyles<typeof styles>, InjectedIntlProps, WithFormContext, WithWidth {
+  onSubmit: () => Promise<any>
+  label?: string
+}
+
 class Submit extends React.Component<Props, State> {
   state: State = {
     saveAwait: null
@@ -21,29 +39,36 @@ class Submit extends React.Component<Props, State> {
     const { isValid } = this.props
     const { onSubmit } = this.props
 
-
-
     console.log(isValid())
     if (isValid()) {
-      this.setState({saveAwait: onSubmit()})
+      this.setState({ saveAwait: onSubmit() })
     }
   }
 
   public render() {
+    const { classes, width, label = "NEXT" } = this.props
     const { saveAwait } = this.state
-    // remove properties from object
-    const { label = "NEXT", children, intl, isValid, onSubmit, registerField, unregisterField, ...props } = this.props
 
-    return (
-      <Grid item xs={12} sm={6}>
-        <Loading await={saveAwait} disableCenter>
-          <Button variant="contained" color="primary" type="submit" onClick={this.handleSubmit} {...props}>
-            <FormattedMessage id={label} />
-          </Button>
-        </Loading>
-      </Grid>
-    )
+    if (isWidthUp('sm', width)) {
+      return (
+        <Grid item xs={12} className={classes.rightAlign}>
+          <Loading await={saveAwait} disableCenter>
+            <Button variant="contained" color="primary" type="submit" onClick={this.handleSubmit}>
+              <FormattedMessage id={label} />
+            </Button>
+            </Loading>
+        </Grid>
+      )
+    } else {
+      return (
+        <Fab className={classes.fab} color="primary" type="submit" onClick={this.handleSubmit}>
+          <Loading await={saveAwait} disableCenter>
+            <ChevronRightIcon />
+          </Loading>
+        </Fab>
+      )
+    }
   }
 }
 
-export default withValidator(injectIntl(Submit))
+export default withValidator(injectIntl(withWidth()(withStyles(styles)(Submit))))
