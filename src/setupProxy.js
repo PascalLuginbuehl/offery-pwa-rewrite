da690d71924af2aa4e4b3e507befae7e3212d10a
pascalluginbuehl <pascal.luginbuehl@bluewin.ch>
2019-12-21 13:57:47 +0100

    Proxy fixed, Translation changed

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.use(
     "/api",
     proxy({
-      target: "https://app.offery.ch/",
+      target: "https://devapp.offery.ch/",
       changeOrigin: true,
     })
   )
