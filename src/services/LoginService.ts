d8b42751df71e53ab82fec91487e0ae2adf8747c
pascalluginbuehl <pascal.luginbuehl@bluewin.ch>
2019-12-28 12:33:03 +0100

    WIP: Refresh token implementation

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -18,36 +18,36 @@ export interface Token {
 class LoginService {
   private static ACCESS_TOKEN_NAME = "BEARER_TOKEN"
 
-  public AuthorizedFetch = async (requestUrl: string, options?: RequestInit): Promise<Response> => {
-    try {
-      // Empty thing correction
-      if (!options) {
-        options = {}
-      }
+  // public AuthorizedFetch = async (requestUrl: string, options?: RequestInit): Promise<Response> => {
+  //   try {
+  //     // Empty thing correction
+  //     if (!options) {
+  //       options = {}
+  //     }
 
-      options.headers = new Headers(options.headers)
+  //     options.headers = new Headers(options.headers)
 
-      // const email = this.email
-      // if (!email) return reject("lol")
-      // options.headers.set("EMAIL", email)
+  //     // const email = this.email
+  //     // if (!email) return reject("lol")
+  //     // options.headers.set("EMAIL", email)
 
-      options.headers.set("Accept", "application/json")
-      options.headers.set("Content-Type", "application/json")
+  //     options.headers.set("Accept", "application/json")
+  //     options.headers.set("Content-Type", "application/json")
 
-    } catch (e) {
-      // TODO Redirect to login
-      throw new Error("Authentication failed")
-    }
+  //   } catch (e) {
+  //     // TODO Redirect to login
+  //     throw new Error("Authentication failed")
+  //   }
 
-    return fetch(API_URL + requestUrl, await this.authorizeRequest(options)).then(errorFunction)
-  }
+  //   return fetch(API_URL + requestUrl, await this.authorizeRequest(options)).then(errorFunction)
+  // }
 
 
-  private set token(value: Token ) {
+  setToken(value: Token ) {
     localStorage.setItem(LoginService.ACCESS_TOKEN_NAME, JSON.stringify(value))
   }
 
-  private get token(): Token {
+  async getToken() {
     const storage = localStorage.getItem(LoginService.ACCESS_TOKEN_NAME)
     if (!storage) throw new Error("Token not set")
 
@@ -57,15 +57,39 @@ class LoginService {
       throw new Error("no Token")
     }
 
-    return token
+    if (token.expires_in > Date.now()) {
+      return await this.refreshToken(token.refresh_token)
+    } else {
+
+      return token.access_token
+    }
+
   }
 
-  private validateAndRefreshToken(token: Token): Promise<string> {
-    return new Promise((resolve, reject) => {
-      // // token.expires_in
-      // // token.refresh_token
-      resolve(token.access_token)
-    })
+  private async refreshToken(oldRefreshToken: string) {
+    try {
+      const user = new URLSearchParams()
+      user.append("refresh_token", oldRefreshToken)
+      user.append("grant_type", "refresh_token")
+
+      const token = await fetch(API_URL + "/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+        body: user.toString(),
+      })
+        .then(errorFunction)
+        .then((response) => response.json())
+      // .then(json => this.toLead(json))
+
+      this.setToken(token)
+
+      return token.access_token
+    } catch (e) {
+      console.error(e)
+      throw new Error("Token refresh failed")
+    }
   }
 
   public async authorizeRequest(options?: RequestInit): Promise<RequestInit> {
@@ -79,7 +103,7 @@ class LoginService {
       options.headers = new Headers(options.headers)
 
       options.headers.set("Accept", "application/json")
-      options.headers.set("Authorization", "Bearer " + (await this.validateAndRefreshToken(this.token)))
+      options.headers.set("Authorization", "Bearer " + await this.getToken())
 
       return options
     } catch (e) {
@@ -111,7 +135,7 @@ class LoginService {
         .then((response) => response.json())
         // .then(json => this.toLead(json))
 
-      this.token = token
+      this.setToken(token)
 
       return
     } catch (e) {
