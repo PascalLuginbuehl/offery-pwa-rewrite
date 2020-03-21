4279115e3507ac187e5cc525a5a961f0ae2752a3
Burim <Burim.cakolli@hotmail.com>
2020-03-21 17:29:42 +0100

    implemented all material order configs

--- src/routes/Lead/Customer/index.tsx
+++ src/routes/Lead/Customer/index.tsx
@@ -101,7 +101,8 @@ class Customer extends React.Component<Props & FormikProps<Values>, {}> {
             <Field name="VisitDate" label="VISITING" component={FormikDateTimePicker} initialFocusedDate={initialDate} />
             {selectedCompany.Settings.EnableServiceMove ? (<Field name="MoveDate" label="MOVING" component={FormikDateTimePicker} initialFocusedDate={VisitDatePlus7} />) : null }
             {selectedCompany.Settings.EnableServicePack ? (<Field name="PackServiceDate" label="PACKINGSERVICE" component={FormikDateTimePicker} initialFocusedDate={MoveDate ? MoveDate : VisitDatePlus7} />) : null }
-            {selectedCompany.Settings.EnableMaterialOrder ? (<Field name="DeliveryDate" label="CARDBOARDBOX_DELIVERY" component={FormikDateTimePicker} initialFocusedDate={VisitDatePlus1} />) : null }
+            {selectedCompany.Settings.EnableMaterialOrder && selectedCompany.Settings.EnableMaterialOrderDelivery ?
+              (<Field name="DeliveryDate" label="CARDBOARDBOX_DELIVERY" component={FormikDateTimePicker} initialFocusedDate={VisitDatePlus1} />) : null }
             {selectedCompany.Settings.EnableServiceStorage ? (<Field name="StorageDate" label="STORAGE" component={FormikDateTimePicker} initialFocusedDate={MoveDate ? MoveDate : VisitDatePlus7} />) : null }
             {selectedCompany.Settings.EnableServiceDisposal ? (<Field name="DisposalDate" label="DISPOSAL" component={FormikDateTimePicker} initialFocusedDate={MoveDate ? MoveDate : VisitDatePlus7} />) : null }
             {selectedCompany.Settings.EnableServiceCleaning ? (<Field name="CleaningDate" label="CLEANING" component={FormikDateTimePicker} initialFocusedDate={MoveDatePlus1} />) : null }
