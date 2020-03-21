50a23e836f3dcabf01124ba014573bc1cf5e4b78
Burim <Burim.cakolli@hotmail.com>
2020-03-21 10:06:03 +0100

    added all company setting properties

--- src/interfaces/ICompany.ts
+++ src/interfaces/ICompany.ts
@@ -20,22 +20,125 @@ export interface IOfferTemplateCategory {
 }
 
 export interface ICompanySetting {
+  //Default Values
+  DefaultServiceTimeStart: number | null
+  DefaultFurnitureLiftPrice: number | null
+  DefaultPianoPrice: number | null
+  DefaultHeavyLiftPrice: number | null
+  DefaultCostPerCubicInMoney: number | null
+  DefaultPaymentMethodTextKey: string
 
-  //Conditiontypes
+  //Conditiontypes Hourly/CostCeiling/Fixprice
+  EnableHourPice: boolean
   EnableDefaultHourPrice: boolean
-  EnableDefaultHasCostCeiling: boolean
   HourlyPriceRangeJSON: number[]
+  EnableCostCeiling: boolean
+  EnableDefaultHasCostCeiling: boolean
+  EnableFixPrice: boolean
+  EnabledPaymentMethodTextKeys: string[]
+  EnableWorkerExpenses: boolean
 
   //Move
+  EnableServiceMove: boolean
+  EnableServiceMoveBore: boolean
+  EnableServiceMoveBoreAmount: boolean
+  EnableServiceMoveBorePrice: boolean
+  EnableServiceMoveMontage: boolean
   EnableServiceMoveMontagePrice: boolean
+  EnableServiceMoveDemontage: boolean
   EnableServiceMoveDemontagePrice: boolean
+  EnableServiceMoveLampDemontage: boolean
+  EnableServiceMoveLampDemontagePrice: boolean
+  EnableServiceMoveFurnitureLift: boolean
+  EnableServiceMoveFurnitureLiftPrice: boolean
+  EnableServiceMoveHeavyLift: boolean
+  EnableServiceMoveHeavyLiftPrice: boolean
+  EnableServiceMovePiano: boolean
+  EnableServiceMovePianoPrice: boolean
+  EnableServiceMoveComment: boolean
 
-  //Storage
-  EnableServiceStorageMontagePrice: boolean
-  EnableServiceStorageDemontagePrice: boolean
+  //Cleaning
+  EnableServiceCleaning: boolean
+  EnableServiceCleaningWorkersAmount: boolean
+  EnableServiceCleaningHandOutGaranty: boolean
+  EnableServiceCleaningHighPressureTerrace: boolean
+  EnableServiceCleaningHighPressureTerracePrice: boolean
+  EnableServiceCleaningFirePlace: boolean
+  EnableServiceCleaningFirePlacePrice: boolean
+  EnableServiceCleaningCarpet: boolean
+  EnableServiceCleaningCarpetPrice: boolean
+  EnableServiceCleaningHighPressureGarage: boolean
+  EnableServiceCleaningHighPressureGaragePrice: boolean
+  EnableServiceCleaningWindows: boolean
+  EnableServiceCleaningWindowsPrice: boolean
+  EnableServiceCleaningWindowsWithShutters: boolean
+  EnableServiceCleaningWindowsWithShuttersPrice: boolean
+  EnableServiceCleaningDovelhole: boolean
+  EnableServiceCleaningDovelholeAmount: boolean
+  EnableServiceCleaningDovelholePrice: boolean
+  EnableServiceCleaningSpecial: boolean
+  EnableServiceCleaningSpecialPrice: boolean
+  EnableServiceCleaningComment: boolean
 
   //Disposal
+  EnableServiceDisposal: boolean
+  EnableServiceDisposalLampDemontage: boolean
+  EnableServiceDisposalLampDemontageAmount: boolean
+  EnableServiceDisposalLampDemontagePrice: boolean
+  EnableServiceDisposalHeavyLift: boolean
+  EnableServiceDisposalHeavyLiftPrice: boolean
+  EnableServiceDisposalDemontage: boolean
   EnableServiceDisposalDemontagePrice: boolean
+  EnableServiceDisposalFurnitureLift: boolean
+  EnableServiceDisposalFurnitureLiftPrice: boolean
+  EnableServiceDisposalComment: boolean
+
+  //Pack
+  EnableServicePack: boolean
+  EnableServicePackOut: boolean
+  EnableServicePackComment: boolean
+
+  //Storage
+  EnableServiceStorage: boolean
+  EnableServiceStorageBore: boolean
+  EnableServiceStorageBoreAmount: boolean
+  EnableServiceStorageBorePrice: boolean
+  EnableServiceStorageMontage: boolean
+  EnableServiceStorageMontagePrice: boolean
+  EnableServiceStorageDemontage: boolean
+  EnableServiceStorageDemontagePrice: boolean
+  EnableServiceStorageLampDemontage: boolean
+  EnableServiceStorageLampDemontageAmount: boolean
+  EnableServiceStorageLampDemontagePrice: boolean
+  EnableServiceStorageFurnitureLift: boolean
+  EnableServiceStorageFurnitureLiftPrice: boolean
+  EnableServiceStorageHeavyLift: boolean
+  EnableServiceStorageHeavyLiftPrice: boolean
+  EnableServiceStoragePiano: boolean
+  EnableServiceStoragePianoPrice: boolean
+  EnableServiceStorageCompanyName: boolean
+  EnableServiceStorageContactPersonFullName: boolean
+  EnableServiceStorageContactPersonTel: boolean
+  EnableServiceStorageContactPersonEMail: boolean
+  EnableServiceStorageComment: boolean
+
+  //Material Order
+  EnableMaterialOrder: boolean
+  EnableMaterialOrderDelivery: boolean
+  EnableMaterialOrderRent: boolean
+  EnableMaterialOrderBuy: boolean
+  EnableMaterialOrderFree: boolean
+  EnableMaterialOrderComment: boolean
+
+  //Buildings
+  CleaningServiceBuildingSetting: IBuildingSetting
+  DisposalServiceBuildingSetting: IBuildingSetting
+  MoveServiceOutBuildingSetting: IBuildingSetting
+  MoveServiceInBuildingSetting: IBuildingSetting
+  PackServiceBuildingSetting: IBuildingSetting
+  StorageServiceInBuildingSetting: IBuildingSetting
+  StorageServiceStorageInBuildingSetting: IBuildingSetting
+  StorageServiceOutBuildingSetting: IBuildingSetting
 
   //Email
   VisitConfirmEmailSubjectTextKey: string
@@ -53,6 +156,38 @@ export interface ICompanySetting {
   AppointmentDisposalDuration: number
   AppointmentCleaningDuration: number
   AppointmentHandOverDuration: number
+}
+
+export interface IBuildingSetting {
+  RoomAmount: boolean
+  TotalArea: boolean
+  StairsToEntryAmount: boolean
+  MetersToParking: boolean
+  PeopleLivingAmount: boolean
+  RestroomAmount: boolean
+  WindowNormalAmount: boolean
+  WindowHighVerticalAmount: boolean
+  HadSmoked: boolean
+  HasMoldAtWindow: boolean
+  HasMoldAtWall: boolean
+  HasHardenedDirt: boolean
+  HadPets: boolean
+  HasBasement: boolean
+  HasAttic: boolean
+  HasGarden: boolean
+  HasWinterGarden: boolean
+  Comment: boolean
+  Etage: boolean
+  Elevator: boolean
+  BuildingAge: boolean
+  BuildingType: boolean
+  BuildingTypeDetail: boolean
+  Balcony: boolean
+  FloorType: boolean
+  RollerBlindType: boolean
+  GarageType: boolean
+  PollutionDegree: boolean
+  BuiltInWardrobeRange: boolean
 }
 
 export interface ICarType {
